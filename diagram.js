3924ce632c666aefe217fad62be4788d4b97a65f
Jackson michalak <jacksonmichalakoficial@gmail.com>
2023-02-27 20:53:07 -0300

    subindo

---
+++ diagram.js
@@ -0,0 +1,61 @@
+// 1 Level
+// body - document.querySelector('body');
+    
+        // 2 Level
+        // h1 - document.querySelector('h1');
+        
+        // div - document.querySelector('.conteudo');
+
+                // 3 Level
+                // h5 - document.querySelector('h5');
+                // ul - document.querySelector('ul');
+
+                        // 4 Level
+                        // li - document.querySelector('#item1');
+                        // li - document.querySelector('#item2');
+                        // li - document.querySelector('#item3');
+        
+        // section - document.querySelector('.card1');
+
+                // 3 level
+                // article - document.querySelector('#card1');
+
+                        // 4 Level
+                        // h2 - document.querySelector('#titleCard1');
+                        // p - document.querySelector('p');
+                
+                // article - document.querySelector('article');
+
+                        // 4 Level
+                        // h2 - document.querySelector('h2');
+                        // p - document.querySelector('p');
+                    
+                // article - document.querySelector('.animado');
+
+                        // 4 Level
+                        // h2 - document.querySelector('h2');
+                        // p - document.querySelector('p');
+
+                // article - document.querySelector('.diferente');
+
+                        // 4 Level
+                        // h2 - document.querySelector('h2');
+                        // p - document.querySelector('p');
+
+                // article - document.querySelector('article');
+
+                        // 4 Level
+                        // h2 - document.querySelector('h2');
+                        // p - document.querySelector('p');
+
+                // article - document.querySelector('article');
+
+                        // 4 Level
+                        // h2 - document.querySelector('h2');
+                        // p - document.querySelector('p');
+
+    
+    
+
+
+
